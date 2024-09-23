9125eca1710971e1af48558109b42b6c6d7f9a62
Zikang Xiong <zikangxiong@icloud.com>
2024-09-23 11:40:29 -0400

    update

---
+++ docs/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'My VuePress Site',
+    description: 'Just playing around',
+    base: '/FLoRA/',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+        ],
+        sidebar: 'auto'
+    }
+}
