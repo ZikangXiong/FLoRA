2cc8855ea1c8280611161a86f65eb14d4f5546fc
ZikangXiong <ZikangXiong@users.noreply.github.com>
2024-09-24 17:38:20 +0000

    deploy: f526595877e8481a4ededfbf090e0b824da6cfcc

--- assets/BibTeX-3YaW_Vhr.js
+++
@@ -1,6 +0,0 @@
-import{_ as a,o as s,c as i,b as n,t as l,F as p}from"./app-SzTRi-4t.js";const u=`@inproceedings{xiong24flora,
-    title={FLoRA: A Framework for Learning Scoring Rules in Autonomous Driving Planning Systems},
-    author={Zikang Xiong, Joe Eappen, and Suresh Jagannathan},
-    booktitle={Preprint},
-    year={2024},
-}`,_={__name:"BibTeX",setup(o,{expose:e}){e();const t={bibtexContent:u};return Object.defineProperty(t,"__isScriptSetup",{enumerable:!1,value:!0}),t}};function c(o,e,t,r,b,d){return s(),i(p,null,[n("div",{class:"bibtex"},[n("pre",null,[n("code",null,l(r.bibtexContent))])]),e[0]||(e[0]=n("br",null,null,-1))],64)}const g=a(_,[["render",c],["__scopeId","data-v-521edc82"],["__file","BibTeX.vue"]]);export{g as default};
---
+++ assets/BibTeX-CpMMHm90.js
@@ -0,0 +1,7 @@
+import{_ as o,o as s,c as i,b as n,t as l,F as c}from"./app-Cq7JVlas.js";const p=`@misc{xiong24flora,
+  title={FLoRA: A Framework for Learning Scoring Rules in Autonomous Driving Planning Systems},
+  author={Xiong, Zikang and Eappen, Joe and Jagannathan, Suresh},
+  year={2024},
+  primaryClass={cs.RO},
+  note={Preprint}
+}`,u={__name:"BibTeX",setup(r,{expose:e}){e();const t={bibtexContent:p};return Object.defineProperty(t,"__isScriptSetup",{enumerable:!1,value:!0}),t}};function _(r,e,t,a,d,b){return s(),i(c,null,[n("div",{class:"bibtex"},[n("pre",null,[n("code",null,l(a.bibtexContent))])]),e[0]||(e[0]=n("br",null,null,-1))],64)}const m=o(u,[["render",_],["__scopeId","data-v-faeec99d"],["__file","BibTeX.vue"]]);export{m as default};
